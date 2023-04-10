a6a3eb1dd3d9ed9d548972fcc3e357cfee5ca979
Matt Carlucci <matthewcarlucci09@gmail.com>
2023-04-09 22:31:08 -0500

    no longer fetch game title for endpoint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import shutil
 import os
-import urllib.request
 import json
 import math
 
@@ -9,9 +8,8 @@ homeDir = os.environ['HOME']
 pluginDir = os.environ['DECKY_PLUGIN_DIR']
 
 class Game:
-    def __init__(self, appid, name, size, size_readable):
+    def __init__(self, appid, size, size_readable):
         self.appid = appid
-        self.name = name
         self.size = size
         self.size_readable = size_readable
 
@@ -46,30 +44,21 @@ class Plugin:
             return size
 
     async def list_games_with_temp_data(self, dirName, sortBy = 'size'):
-        # try to fetch GetAppList url, otherwise fallback to static data (poor man's offline mode)
-        try:
-            response = urllib.request.urlopen('https://api.steampowered.com/ISteamApps/GetAppList/v0002/')
-        except:
-            response = open(pluginDir + '/GetAppListV0002.json')
-        
-        all_games = json.loads(response.read())['applist']['apps']
         # list of game appids on steam deck
-        local_game_ids = await self._listdirs(self, homeDir + '/.steam/steam/steamapps/' + dirName)
-        # filter all_games by local games appid
-        games_found = list(filter(lambda d: str(d['appid']) in local_game_ids, all_games))
+        local_games = await self._listdirs(self, homeDir + '/.steam/steam/steamapps/' + dirName)
 
         games_list = []
-        for game in games_found:
-            dir_name = dirName + '/' + str(game['appid'])
+        for appid in local_games:
+            dir_name = dirName + '/' + str(appid)
             size = await self.get_size(self, dir_name)
             size_readable = self._convert_size(self, size)
-            game = Game(game['appid'], game['name'], size, size_readable)
+            game = Game(appid, size, size_readable)
             games_list.append(game)
 
         # sort list by game property, default to size (desc)
-        sorted_games_found = sorted([obj.__dict__ for obj in games_list], key=lambda x: x[sortBy], reverse=True)
+        sorted_local_games = sorted([obj.__dict__ for obj in games_list], key=lambda x: x[sortBy], reverse=True)
 
-        return json.dumps(sorted_games_found)
+        return json.dumps(sorted_local_games)
 
     async def delete_cache(self, dirName):
         await shutil.rmtree(homeDir + '/.steam/steam/steamapps/' + dirName)
